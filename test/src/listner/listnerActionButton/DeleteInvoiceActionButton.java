4bba8bd605b16f68d78876bb952b28811d8a5c09
KarimTaha <102817729+karim-taha0@users.noreply.github.com>
2022-04-14 23:35:15 +0200

    Udacity | egFWD | Sales Invoices Generator Project | To generate a simple sales invoice using java , oop , java GUI

---
+++ test/src/listner/listnerActionButton/DeleteInvoiceActionButton.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listner.listnerActionButton;
+
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import salesinvoicegenerator.Frame;
+
+/**
+ *
+ * @author karim
+ */
+public class DeleteInvoiceActionButton {
+
+    public DeleteInvoiceActionButton(Frame frame) {
+        frame.headerModel = (DefaultTableModel) frame.invoicesTable.getModel();
+        if (frame.invoicesTable.getSelectedRowCount() == 1) { //if you already select a row
+            frame.headerModel.removeRow(frame.invoicesTable.getSelectedRow());            // remove selected row from the model
+            JOptionPane.showMessageDialog(null, "Selected row deleted successfully");
+        } else {
+            if (frame.invoicesTable.getRowCount() == 0) {
+                JOptionPane.showMessageDialog(null, "Empty Table, please load a txt file");
+            } else {
+                JOptionPane.showMessageDialog(null, "Select a row to delete");
+
+            }
+        }
+    }
+}
---
+++ test/src/salesinvoicegenerator/Main.java
@@ -0,0 +1,12 @@
+package salesinvoicegenerator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // TODO code application logic here
+        new Frame().setVisible(true);
+
+    }
+
+}
