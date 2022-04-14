6be077883d92bba3cea1e7dd204976988e90b08a
KarimTaha <102817729+karim-taha0@users.noreply.github.com>
2022-04-14 23:44:46 +0200

    Udacity | egFWD | Sales Invoices Generator Project | To generate a simple sales invoice using java , oop , java GUI

--- src/listner/listnerMenuBar/LoadInvoiceActionListner.java
+++ src/listner/listnerMenuBar/LoadInvoiceActionListner.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package listner.listnerMenuBar;
+package listner.listnerActionButton;
 
 import salesinvoicegenerator.Frame;
 import java.io.BufferedReader;
